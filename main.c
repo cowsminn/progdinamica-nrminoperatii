a3de9c31faac672145e8e7ae3e1c637080a70106
cowsmin <cosmin.mnj@yahoo.com>
2023-11-08 21:00:27 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,43 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 
-void egipt(int a, int b)
-{
-    assert(b != 0 && "nedifinit - numitorul == 0"); /// nu am stiut exact cum as putea sa folosesc
-                                                    /// assert pt outputul de la void() recursiv
-    if(a == 0){
-        printf("0");
-        return;
+void secventa(int x, int anterior[]) {
+    if (x > 1) {
+        if (anterior[x] == 1)
+            {secventa(x - 1, anterior);
+            printf("(+1) -> ");
+            }
+        else if (anterior[x] == 2)
+            {secventa(x / 2, anterior);
+            printf("(*2) -> ");
+            }
+        else
+            {secventa(x / 3, anterior);
+            printf("(*3) -> ");
+            }
     }
-    if (b % a == 0) {
-        printf("1/%d", b / a);
-        return;
-    }
-    if (a % b == 0) {
-        printf("%d", a / b);
-        return;
-    }
-    if (a > b) {
-        printf("%d + ", a / b);
-        egipt(a % b, b);
-        return;
-    }
-    int x = b / a + 1;
-    printf("1/%d + ", x);
-    egipt(a * x - b, b * x);
+    printf("%d", x);
 }
-int main()
-{
-    int a, b; /// a - numaratorul b - numitorul
 
-    printf("Numaratorul: ");
-    scanf("%d", &a);
-    printf("Numitorul: ");
-    scanf("%d", &b);
-    printf("Fractia %d/%d se scrie in fractii egiptene astfel:\n", a, b);
+void func(int x) {
+    int nr_operatii[x+1];
+    int anterior[x+1];
+    nr_operatii[1] = 0;
 
-    egipt(a, b);
+    for (int i = 2; i <= x; i++) {
+        nr_operatii[i] = nr_operatii[i-1] + 1;
+        anterior[i] = 1;
 
+        if (i % 2 == 0 && nr_operatii[i/2] + 1 < nr_operatii[i]) {
+            nr_operatii[i] = nr_operatii[i/2] + 1;
+            anterior[i] = 2;
+        }
+
+        if (i % 3 == 0 && nr_operatii[i/3] + 1 < nr_operatii[i]) {
+            nr_operatii[i] = nr_operatii[i/3] + 1;
+            anterior[i] = 3;
+        }
+    }
+
+    printf("numarul de operatii minime pentru obtinerea numarului %d este: %d\n", x, nr_operatii[x]);
+
+    printf("valori intermediare: ");
+    secventa(x, anterior);
+    printf("\n");
+}
+
+int main() {
+    int x;
+    FILE* f = fopen("date.in","r");
+    if(f == NULL)
+    {
+        printf("eroare fisier");
+        fclose(f);
+    }
+    fscanf(f, "%d", &x);
+    func(x);
+    fclose(f);
     return 0;
 }
